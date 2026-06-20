d02bbdbe214918abb1a29137c55c5eae7932d07e
Michelle G. <michellegalvan9909@gmail.com>
2026-06-19 19:31:55 -0700

    leccion 1 and 2

---
+++ leccion_2/main.py
@@ -0,0 +1,13 @@
+# Create a variable input that asks for birthday but also has the nombre variable in the question
+
+
+print("Bienvenidos a la cafeteria ")
+nombre=input("?Cual es tu nombre? ") # Eg: Nick
+# REPLACE ME ADD MISSING VARIABLE AND INPUT HERE 
+
+cumpleanios=input("que haras para tu cumpleanios?") # Eg: June 17 1998
+# print(nombre + " I will not for get your birthday, I'll get the party going for " + cumpleanios) # FIll IN the birthday variable you create
+print(nombre + "no estare para tu cumpleanios, ire a otro evento" + cumpleanios)
+
+
+
